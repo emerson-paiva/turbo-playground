ee0ee2f41b06e9e3f46c95dd0f29bf0c8e278626
Emerson Paiva <emersonaraujopaiva@gmail.com>
2023-04-21 08:18:17 -0300

    ci: test using standalone

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   reactStrictMode: true,
   experimental: {
-    // this includes files from the monorepo base two directories up
+    outputStandalone: true,
     outputFileTracingRoot: path.join(__dirname, '../../'),
   },
   transpilePackages: ['ui'],
