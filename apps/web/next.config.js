858dffa8403b17f2d9fe27c1a5554f617e60a386
Emerson Paiva <emersonaraujopaiva@gmail.com>
2023-04-21 08:06:40 -0300

    ci: change next config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   reactStrictMode: true,
+  experimental: {
+    // this includes files from the monorepo base two directories up
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+  },
   transpilePackages: ['ui'],
 };
